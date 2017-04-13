5a4c9d078c77b7be9b7867b2f8df8bf656d8f381
Simon Zhen <obersink@gmail.com>
2017-04-12 21:48:58 -0700

    layout fixes and added gitignore

---
+++ WeatherApp/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Simon Zhen on 4/12/17.
+//  Copyright © 2017 Simon Zhen. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
--- WeatherApp/WeatherVC.swift
+++ WeatherApp/WeatherVC.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+}
+
+extension WeatherVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        return cell
+    }
+    
 }
 
