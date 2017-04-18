e8fb157426228d4eb901a7866032652c009123cb
Simon Zhen <obersink@gmail.com>
2017-04-18 01:53:07 -0700

    TODO: weathercell

--- WeatherApp/Forecast.swift
+++ WeatherApp/Forecast.swift
@@ -10,10 +10,13 @@ import UIKit
 import Alamofire
 
 class Forecast {
+    
     var _date: String!
     var _weatherType: String!
     var _highTemp: String!
     var _lowTemp: String!
+    
+    static var forecast = [Forecast]()
     
     var date: String {
         if _date == nil {
@@ -48,15 +51,15 @@ class Forecast {
             if let min = temp["temp_min"] as? Double {
                 self._lowTemp = "\(min)"
             }
+            
             if let max = temp["temp_max"] as? Double {
                 self._highTemp = "\(max)"
             }
-
         }
         
         if let weather = weatherDict["weather"] as? [Dictionary<String, AnyObject>] {
-            if let main = weather[0]["main"] as? String {
-                
+            if let weatherType = weather[0]["main"] as? String {
+                self._weatherType = weatherType
             }
         }
         
@@ -67,31 +70,37 @@ class Forecast {
             dateFormatter.dateFormat = "EEEE"
             dateFormatter.timeStyle = .none
             self._date = unixConvertedDate.dayOfTheWeek()
-            print(self.date)
         }
-        
     }
     
-    class func downloadForecast(completed: DownloadComplete) {
+    class func getForecasts() -> [Forecast] {
+        return forecast
+    }
+    
+    class func downloadForecast(completed: @escaping DownloadComplete) {
         let forecastURL = URL(string: FORECAST_URL)!
+        
         Alamofire.request(forecastURL).responseJSON { response in
             let result = response.result
+            var list = [Forecast]()
+            
             if let dict = result.value as? Dictionary<String,AnyObject> {
                 if let forecastList = dict["list"] as? [Dictionary<String, AnyObject>] {
                     
                     for f in forecastList {
                         let fd = Forecast(weatherDict: f)
+                        list.append(fd)
                     }
-                    
                 }
             }
+            forecast = list
+            completed()
         }
-        print(forecastURL)
     }
-    
 }
 
 extension Date {
+    
     func dayOfTheWeek() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE"
